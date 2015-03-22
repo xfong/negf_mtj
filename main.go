b44cb356b825699f3a5851c096e70c44c4b5a498
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2015-03-22 08:15:55 -0400

    Added code for calculating main diagonal of Hamiltonian

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "math"
     "math/cmplx"
     "fmt"
+    "github.com/negf_mtj/negf_mtj/cmplxSparse"
 )
 
 var (
@@ -38,11 +39,16 @@ func main() {
     m_ox := float64(0.315);
     m_fm_L := float64(0.72);
     m_fm_R := m_fm_L;
-
+/*
     aSpace := float64(1.0e-11);
     d_ox := float64(1.15e-9);
     d_fm_L := float64(2.0e-11);
     d_fm_R := float64(2.0e-11);
+*/
+    aSpace := float64(0.25e-9);
+    d_ox := float64(1.25e-9);
+    d_fm_L := float64(0.25e-9);
+    d_fm_R := float64(0.25e-9);
 
     N_fm_L := int(d_fm_L/aSpace) - 1;
     N_fm_R := int(d_fm_R/aSpace) - 1;
@@ -52,4 +58,27 @@ func main() {
     t_fm_L := t_base/m_fm_L;
     t_fm_R := t_base/m_fm_R;
     t_ox := t_base/m_ox;
+
+    // Initialize a base matrix template
+    Hamiltonian := cmplxSparse.New();
+    cmplxSparse.MakeHamTriDiag(N_fm_L+N_fm_R+N_ox+2,Hamiltonian);
+
+    // Build the main diagonal first
+    cmplxSparse.ScaleRangeSparseMatrixIP(0, 2*(N_fm_L)-1, 0, complex(t_fm_L, 0.0), Hamiltonian); // Left FM contact
+    cmplxSparse.ScaleRangeSparseMatrixIP(2*N_fm_L, 2*N_fm_L+1, 0, complex(0.5*(t_fm_L+t_ox), 0.0), Hamiltonian); // Interface of left FM contact with barrier
+    cmplxSparse.ScaleRangeSparseMatrixIP(2*(N_fm_L+1), 2*(N_fm_L+N_ox)-1, 0, complex(t_ox, 0.0), Hamiltonian); // Barrier
+    cmplxSparse.ScaleRangeSparseMatrixIP(2*(N_fm_L+N_ox), 2*(N_fm_L+N_ox)+1, 0, complex(t_ox, 0.0), Hamiltonian); // Interface of right FM contact with barrier
+    cmplxSparse.ScaleRangeSparseMatrixIP(2*(N_fm_L+N_ox+1), 2*(N_fm_L+N_ox+N_fm_R)+3, 0, complex(t_fm_R, 0.0), Hamiltonian); // Right FM contact
+
+/*
+    // Build the upper diagonal
+    cmplxSparse.ScaleRangeSparseMatrixIP(0, N_fm_L-1, 2, complex(t_fm_L, 0.0), Hamiltonian);    
+    cmplxSparse.ScaleRangeSparseMatrixIP(N_fm_L, N_fm_L+N_ox-1, 2, complex(t_ox, 0.0), Hamiltonian);   
+    cmplxSparse.ScaleRangeSparseMatrixIP(N_fm_L+N_ox, N_fm_L+N_ox+N_fm_R-1, 2, complex(t_fm_R, 0.0), Hamiltonian);    
+
+    // Build the upper diagonal
+    cmplxSparse.ScaleRangeSparseMatrixIP(2, N_fm_L+1, -2, complex(t_fm_L, 0.0), Hamiltonian);    
+    cmplxSparse.ScaleRangeSparseMatrixIP(N_fm_L+2, N_fm_L+N_ox+1, -2, complex(t_ox, 0.0), Hamiltonian);   
+    cmplxSparse.ScaleRangeSparseMatrixIP(N_fm_L+N_ox+1, N_fm_L+N_ox+N_fm_R+1, -2, complex(t_fm_R, 0.0), Hamiltonian);    
+*/
 }
