4686cda297794d6198e706cf2fd1c54228de9e77
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2015-04-03 03:42:41 -0400

    Include two additional scaling factors for calculating currents

--- utils/const.go
+++ utils/const.go
@@ -7,14 +7,16 @@ import (
 )
 
 var (
-    Pi		= float64(4.0 * math.Atan(1.0));
-    K_B		= float64(1.3806488e-23);
-    Hplanck	= float64(6.62606957e-34);
-    Hbar	= Hplanck/2.0/Pi;
-    Echarge	= float64(1.60217657e-19);
-    Mu0		= Pi*4.0e-7;
-    MuB		= float64(9.27400968e-24);
-    Zplus	= 1e-9;
-    M0		= float64(9.10938291e-31);
-    K_q		= K_B / Echarge;
+    Pi		    = float64(4.0 * math.Atan(1.0));
+    K_B		    = float64(1.3806488e-23);
+    Hplanck	    = float64(6.62606957e-34);
+    Hbar	    = Hplanck/2.0/Pi;
+    Echarge	    = float64(1.60217657e-19);
+    Mu0		    = Pi*4.0e-7;
+    MuB		    = float64(9.27400968e-24);
+    Zplus	    = 1e-9;
+    M0		    = float64(9.10938291e-31);
+    K_q		    = K_B / Echarge;
+    ECurrFactor = Echarge * Echarge / Hplanck;
+    SCurrFactor = ECharge / (4*Pi);
 )
