8a2d6354ccb3b6ac6d9ccc81c22c46a26d4df6dd
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2015-04-04 09:30:04 -0400

    Added tests for LU factorization and matrix inversion

--- test/test_LU.go
+++ test/test_LU.go
@@ -12,6 +12,9 @@ func main() {
 	// Test LU factorization...
 	testLU(6);
 
+	// Test diagonal matrix inversion...
+	testDiagInversion(3);
+
 }
 
 
@@ -36,5 +39,40 @@ func testLU( n int ) {
                 }
                 fmt.Printf("\n");
         }
+	fmt.Println("\n");
+}
+
+func testDiagInversion( n int ) {
+    fmt.Println("Initializing Sparse matrix structure\n");
+    tmp := cmplxSparse.New();
+
+    matrixSize := int(2*n);
+    fmt.Println("Making sparse ",matrixSize,"x",matrixSize," Hamiltonian tridiagonal matrix\n");
+    cmplxSparse.MakeHamTriDiag(n, tmp);
+	for idx0 := 0; idx0 < matrixSize; idx0++ {
+		tmp.Data[idx0][2] -= complex(0.0,5.0);
+	}
+
+    InvBuffer := make([][]complex128, matrixSize);
+    BVector := make([]complex128, matrixSize);
+    for idx0 := 0; idx0 < matrixSize; idx0++ {
+        BVector[idx0] = 0;
+    }
+    for idx0 := 0; idx0 < matrixSize; idx0++ {
+        if (idx0 > 0) {
+            BVector[idx0-1] = 0.0 + 0.0i;
+        }
+        BVector[idx0] = 1;
+        InvBuffer[idx0] = make([]complex128, matrixSize);
+        InvBuffer[idx0] = SparseDiagLinearSolver(tmp, BVector);
+    }
+    fmt.Println("Accessing matrix elements (m,n):");
+
+    for idx0 := 0; idx0 < matrixSize; idx0++ {
+        for idx1 := 0; idx1 < matrixSize; idx1++ {
+            fmt.Printf("%f  ", InvBuffer[idx1][idx0]);
+        }
+        fmt.Printf("\n");
+    }
 	fmt.Println("\n");
 }
